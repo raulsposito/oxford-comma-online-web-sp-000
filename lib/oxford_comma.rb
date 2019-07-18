17777cfc998fd8d7502387f4be96dd1233b4a4a2
Raul Sposito <raulsposito@gmail.com>
2019-07-18 22:35:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
-def oxford_comma(array)
-  case array.length <= 1 
-  ["kiwi", "durian", "starfruit"].join
-  elsif array.length == 2 
-  ["kiwi", "durian", "starfruit"].join(" and ")
-  elsif array.length >= 3
-  ["kiwi", "durian", "starfruit"].join(", "" and ")
-  end
-end
+#def oxford_comma(array)
+#  case array.length <= 1 
+#  ["kiwi", "durian", "starfruit"].join
+#  elsif array.length == 2 
+#  ["kiwi", "durian", "starfruit"].join(" and ")
+#  elsif array.length >= 3
+# # ["kiwi", "durian", "starfruit"].join(", "" and ")
+#  end
+#end
 
 #def english_join(array = nil)
 #  return array.to_s if array.nil? or array.length <= 1
