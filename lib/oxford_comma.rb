9a99db525cf895ffcb824421ea960ca2773100ff
Raul Sposito <raulsposito@gmail.com>
2019-07-18 22:24:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,16 +5,10 @@ def oxford_comma(array)
   when 2
     string = array[0] + " and " + array[1]
   else
-    string
-    
-
-
-
-def oxford_comma(array)
-  case array.size
-  when 1
-    return array[0].to_s
-  when 2
-    string = array[0] + " and " + array[1]
-  else
-    string
+    string = String.new
+    array.each_with_index do |item, index|
+      string += item + ", "
+    end 
+    return string
+  end 
+end
